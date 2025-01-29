8a460e54788590886bb3603e17fc0df87464bed7
paulsymon <paul.rocha@ifpi.edu.br>
2025-01-29 20:45:29 -0300

    Add images for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,8 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Diretório onde os arquivos serão coletados
+#STATIC_ROOT = BASE_DIR/'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Diretório para uploads de imagens dos usuários
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
